f15e1cff618f520ad78e12ba82932f9574a42329
Xurxe <xurxe.garcia@integrify.io>
2019-03-07 12:24:04 +0200

    Created array with planets as objects, and name/density/densityRange as properties

--- script.js
+++ script.js
@@ -1,3 +1,151 @@
+/*
+// planet densities in g/cm^3 (lowest to highest)
+const densitySaturn = 0.69;
+const densityUranus = 1.27;
+const densityJupiter = 1.33;
+const densityNeptune = 1.64;
+const densityMars = 3.93;
+const densityVenus = 5.24;
+const densityMercury = 5.42;
+const densityEarth = 5.51;
+*/
+
+
+
+/* 
+// manual calculation of density ranges
+// using the middle point between the density of one planet and the next as the breaking point in ranges
+
+const densityRangeSaturn = [
+    0, 
+    densitySaturn + 0.5 * (densityUranus - densitySaturn),
+];
+
+const densityRangeUranus = [
+    densityUranus - 0.5 * (densityUranus - densitySaturn), 
+    densityUranus + 0.5 * (densityJupiter - densityUranus),
+];
+
+const densityRangeJupiter = [
+    densityJupiter - 0.5 * (densityJupiter - densityUranus), 
+    densityJupiter + 0.5 * (densityNeptune - densityJupiter),
+];
+
+const densityRangeNeptune = [
+    densityNeptune - 0.5 * (densityNeptune - densityJupiter), 
+    densityNeptune + 0.5 * (densityMars - densityNeptune),
+];
+
+const densityRangeMars = [
+    densityMars - 0.5 * (densityMars - densityNeptune), 
+    densityMars + 0.5 * (densityVenus - densityMars),
+];
+
+const densityRangeVenus = [
+    densityVenus - 0.5 * (densityVenus - densityMars), 
+    densityVenus + 0.5 * (densityMercury - densityVenus),
+];
+
+const densityRangeMercury = [
+    densityMercury - 0.5 * (densityMercury - densityVenus), 
+    densityMercury + 0.5 * (densityEarth - densityMercury),
+];
+
+const densityRangeEarth = [
+    densityEarth - 0.5 * (densityEarth - densityMercury), 
+    Infinity,
+];
+*/
+
+
+// define an array (planets). each element is an object (planet).
+// each object corresponds to a planet in the solar system; properties are the name and density of the planet.
+// planets are ordered from lowest to highest density
+const planets = [
+    {
+        name: 'Saturn',
+        density: 0.69,
+    },
+
+    {
+        name: 'Uranus',
+        density: 1.27,
+    },
+
+    {
+        name: 'Jupiter',
+        density: 1.33,
+    },
+
+    {
+        name: 'Neptune',
+        density: 1.64,
+    },
+
+    {
+        name: 'Mars',
+        density: 3.93,
+    },
+
+    {
+        name: 'Venus',
+        density: 5.24,
+    },
+
+    {
+        name: 'Mercury',
+        density: 5.42,
+    },
+
+    {
+        name: 'Earth',
+        density: 5.51,
+    },
+]
+
+
+// define function to generate density ranges
+function addDensityRanges(array) {
+
+    // for the planet with the lowest density, the density range is
+    // from: 0
+    // to: the middle point between its density and the density of the next planet
+    array[0].densityRange = [
+        0, 
+        array[0].density + 0.5 * (array[1].density - array[0].density)
+    ];
+
+    // for most other planets, the density range is
+    // from: the middle point between its density and the density of the previous planet
+    // to: the middle point between its density and the density of the next planet
+
+    for (let i = 1; i < array.length - 1; i++){
+        array[i].densityRange = [
+            array[i].density - 0.5 * (array[i].density - array[i - 1].density),
+            array[i].density + 0.5 * (array[i + 1].density - array[i].density)
+        ];
+    }
+
+    // for the planet with the highest density, the density range is 
+    // from: the middle point between its density and the density of the previous planet
+    // to: infinity
+    let n = array.length - 1;
+
+    array[n].densityRange = [
+        array[n].density - 0.5 * (array[n].density - array[n - 1].density),
+        Infinity
+    ];
+
+    // return array
+    return array;
+
+};
+
+// execute the function on the planets-array
+addDensityRanges(planets);
+
+
+
 // select the calculation button
 const calculateDensityButton = document.querySelector('#calculateDensityButton');
 
@@ -39,14 +187,13 @@ calculateDensityButton.addEventListener('click', function() {
     
 
 
-    // if the result is valid (a number), write the result below in the HTML
-    if (typeof density === 'number') {
-        calculateDensityResult.innerHTML = `The density of your object is ${density} ${unitMassLabel} / ${unitVolumeLabel}.`;
-    }
+    // write a sentence with the density of the object
+    calculateDensityResult.innerHTML = `The density of your object is ${density} ${unitMassLabel} / ${unitVolumeLabel}.`;
 
-    // otherwise, write error
-    else {
-        calculateDensityResult.innerHTML = `Error.`;
-    }
+
+
+    // write a sentence comparing the density of the object to the density of a planet
+    compareToPlanet.innerHTML = `The density of your object is closest to the density of... .`;
+
 
 });
